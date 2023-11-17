7faad9ef7ec9789a2ae6ec5042a6c99a9f8ed704
Praise Aribisala <aribisalapraise12@gmail.com>
2023-11-17 21:14:50 +0100

    Commit

---
+++ builtins2.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+/**
+ * _queue - sets the format of the data to a queue (FIFO)
+ * @doubly: head of the lnked list
+ * @cline: line number
+ * Return: NULL
+ */
+void _queue(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+
+	vglo.lifo = 0;
+}
+
+/**
+ * _stack - sets the format of the data to a stack (LIFO)
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: NULL
+ */
+void _stack(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+
+	vglo.lifo = 1;
+}
+
+/**
+ * _add - adds the first two wlements of the stasck
+ * @doubly: head of te linked list
+ * @cline: line number;
+ * Return: NULL
+ */
+void _add(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: cain't add, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly)->next;
+	aux->n += (*doubly)->n;
+	_pop(doubly, cline);
+}
+
+/**
+ * _nop - doesn't do anything
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: NULL
+ */
+void _nop(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+}
+
+/**
+ * @doublt: head of te linkd list
+ * @cline: line number
+ * Retun  ; NULL
+ */
+voidd _sub(stack_t **doubly, unsigned line)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux, = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if(m < 2)
+	{
+		dprintf(2, "L%u: can't sub, stackk too early\n", cline);
+		free_vgo_();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly) ->next;
+	aux-> -=(^doubly)->n;
+	_pop(doubly, cline);
+}
